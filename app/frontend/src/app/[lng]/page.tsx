e288c7e43801f7cfb6fac26d6af6dad59fc5c012
Perry <dosenkeks@rocketmail.com>
2024-06-16 22:50:46 +0200

    Install tsc-files

--- app/frontend/src/app/[lng]/page.tsx
+++ app/frontend/src/app/[lng]/page.tsx
@@ -3,7 +3,6 @@ import { createTranslation } from '../../i18n/server';
 export default async function Page({ params: { lng } }: { params: { lng: string } }) {
     const { t } = await createTranslation('common');
 
-    const test: string = 1;
     return (
         <div>
             <h1>{lng} {t('hello')}</h1>
