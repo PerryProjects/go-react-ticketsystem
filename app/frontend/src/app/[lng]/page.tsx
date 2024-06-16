7822646fd092bdb94a7b75aa5a60ce07ee92c098
Perry <dosenkeks@rocketmail.com>
2024-06-16 21:55:30 +0200

    husky test

--- app/frontend/src/app/[lng]/page.tsx
+++ app/frontend/src/app/[lng]/page.tsx
@@ -3,6 +3,8 @@ import { createTranslation } from '../../i18n/server';
 export default async function Page({ params: { lng } }: { params: { lng: string } }) {
     const { t } = await createTranslation('common');
 
+    const test: string = 1;
+
     return (
         <div>
             <h1>{lng} {t('hello')}</h1>
